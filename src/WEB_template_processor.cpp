e622650d94e0169cfc47922a495906e8a58c6e21
cdntinker <cdntinker@gmail.com>
2021-06-16 20:59:39 -0400

    8

--- src/WEB_template_processor.cpp
+++ src/WEB_template_processor.cpp
@@ -64,7 +64,7 @@ ButtonMap Button[] =
         {"/management", "Management"},
 };
 int NumberofButtons = 3;
-extern char* CurrentPage;
+extern char CurrentPage[32];
 /**********************************************************************/
 
 String processor(const String &var) // Change placeholders on webpage
