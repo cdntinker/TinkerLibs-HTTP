c4cebf7a1ca9e0525ef2cee94fcd8d9a28173944
cdntinker <cdntinker@gmail.com>
2021-06-17 14:22:58 -0400

    666

--- src/WEB_template_processor.cpp
+++ src/WEB_template_processor.cpp
@@ -151,7 +151,7 @@ String processor(const String &var) // Change placeholders on webpage
         {
             // if (Button[ButtonCTR].ButtonAddress == CurrentAddress)
             // if (!strcmp(Button[ButtonCTR].ButtonAddress, "/test"))
-            if (!strcmp(Button[ButtonCTR].ButtonAddress, CurrentPage))
+            if (strcmp(Button[ButtonCTR].ButtonAddress, CurrentPage))
             {
                 ButtonClass = "ButtonHere";
             }
