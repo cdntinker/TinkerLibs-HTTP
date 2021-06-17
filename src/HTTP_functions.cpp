ba5c10bad54bb04b5a9a2cbe0537b85c3c3d1b8d
cdntinker <cdntinker@gmail.com>
2021-06-17 14:16:07 -0400

    8

--- src/HTTP_functions.cpp
+++ src/HTTP_functions.cpp
@@ -59,9 +59,8 @@ boolean setup_HTTP()
  * *******************************************************/
 #define InitializePage(PageNumber)                                                            \
     server.on(PageContent[PageNumber].Address, HTTP_GET, [](AsyncWebServerRequest *request) { \
-    response->addHeader("Server","ESP Async Web Server"); \
-        request->send_P(200, "text/html", PageContent[PageNumber].p_Content, processor);      \
         strcpy(CurrentPage, PageContent[PageNumber].Address);     \
+        request->send_P(200, "text/html", PageContent[PageNumber].p_Content, processor);      \
         sprintf(DEBUGtxt, "Page: %s accessed", PageContent[PageNumber].Address);              \
         DEBUG_LineOut(DEBUGtxt);                                                              \
     });                                                                                       \
