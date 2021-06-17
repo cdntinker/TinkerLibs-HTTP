ee9d40fc612cb50e635e0bb6990faa0db08ddc59
cdntinker <cdntinker@gmail.com>
2021-06-17 14:33:43 -0400

    8

--- src/WEB_template_processor.cpp
+++ src/WEB_template_processor.cpp
@@ -92,41 +92,41 @@ String processor(const String &var) // Change placeholders on webpage
             return "Testing";
         }
 
-        // if (var == "WT_Button_0_Type")
-        // {
-        //     return "Button_0_Type";
-        // }
-        // if (var == "WT_Button_0_Address")
-        // {
-        //     return "Button_0_Address";
-        // }
-        // if (var == "WT_Button_0_Label")
-        // {
-        //     return "Button_0_Label";
-        // }
+        if (var == "WT_Button_0_Type")
+        {
+            return "Button_0_Type";
+        }
+        if (var == "WT_Button_0_Address")
+        {
+            return "Button_0_Address";
+        }
+        if (var == "WT_Button_0_Label")
+        {
+            return "Button_0_Label";
+        }
 
-        // if (var == "WT_Button_1_Type")
-        // {
-        //     return "Button_1_Type";
-        // }
-        // if (var == "WT_Button_1_Address")
-        // {
-        //     return "Button_1_Address";
-        // }
-        // if (var == "WT_Button_1_Label")
-        // {
-        //     return "Button_1_Label";
-        // }
+        if (var == "WT_Button_1_Type")
+        {
+            return "Button_1_Type";
+        }
+        if (var == "WT_Button_1_Address")
+        {
+            return "Button_1_Address";
+        }
+        if (var == "WT_Button_1_Label")
+        {
+            return "Button_1_Label";
+        }
 
-        // if (var == "WT_Button_2_Type")
-        // {
-        //     return "Button_2_Type";
-        // }
-        // if (var == "WT_Button_2_Address")
-        // {
-        //     return "Button_2_Address";
-        // }
-        // if (var == "WT_Button_2_Label")
+        if (var == "WT_Button_2_Type")
+        {
+            return "Button_2_Type";
+        }
+        if (var == "WT_Button_2_Address")
+        {
+            return "Button_2_Address";
+        }
+        if (var == "WT_Button_2_Label")
         {
             return "Button_2_Label";
         }
