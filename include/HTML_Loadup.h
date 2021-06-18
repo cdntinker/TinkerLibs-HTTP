c99cacfd5fff31617154912d1a19b418d2285cca
cdntinker <cdntinker@gmail.com>
2021-06-17 23:12:50 -0400

    8

--- include/HTML_Loadup.h
+++ include/HTML_Loadup.h
@@ -46,42 +46,37 @@ default:
 #include "HTML/Parts/Content_WTF.htm"
     break;
 }
-// #if TestPage == 0
-// #include "HTML/Parts/Content_Verkz.htm"
-// #elif TestPage == 1
-// #include "HTML/Parts/Content_Poem.htm"
-// #elif TestPage == 2
-// #include "HTML/Parts/Content_SiniLink.htm"
-// #elif TestPage == 3
-// #include "HTML/Parts/Content_DeviceInfo.htm"
-// #elif TestPage == 4
-// #include "HTML/Parts/Content_I2C.htm"
-// #elif TestPage == 5
-// #include "HTML/Parts/Content_Sonoff.htm"
-// #else
-// #include "HTML/Parts/Content_WTF.htm"
-// #endif
 #else
 #include "HTML/Parts/Content_Oops.htm"
 #endif
 
 const char *HomePagePart PROGMEM =
 #if defined(HomePage)
-#if HomePage == 0
+    switch (TestPage)
+{
+case 0:
 #include "HTML/Parts/Content_Verkz.htm"
-#elif HomePage == 1
+    break;
+case 1:
 #include "HTML/Parts/Content_Poem.htm"
-#elif HomePage == 2
+    break;
+case 2:
 #include "HTML/Parts/Content_SiniLink.htm"
-#elif HomePage == 3
+    break;
+case 3:
 #include "HTML/Parts/Content_DeviceInfo.htm"
-#elif HomePage == 4
+    break;
+case 4:
 #include "HTML/Parts/Content_I2C.htm"
-#elif HomePage == 5
+    break;
+case 5:
 #include "HTML/Parts/Content_Sonoff.htm"
-#else
+    break;
+
+default:
 #include "HTML/Parts/Content_WTF.htm"
-#endif
+    break;
+}
 #else
 #if defined(I2Cenable)
 #include "HTML/Parts/Content_I2C.htm"
