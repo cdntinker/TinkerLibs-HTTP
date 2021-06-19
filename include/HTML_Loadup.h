cf4a1ed2c1d56e5173d3db408b402aa277a5ea49
cdntinker <cdntinker@gmail.com>
2021-06-19 13:50:37 -0400

    8

--- include/HTML_Loadup.h
+++ include/HTML_Loadup.h
@@ -50,40 +50,19 @@ Content_WTF;
 Content_Oops;
 #endif
 
-// const char *TestPagePart PROGMEM =
-// #if defined(TestPage)
-// #if TestPage == 0
-// #include "HTML/Parts/Content_Verkz.htm"
-// #elif TestPage == 1
-// #include "HTML/Parts/Content_Poem.htm"
-// #elif TestPage == 2
-// #include "HTML/Parts/Content_SiniLink.htm"
-// #elif TestPage == 3
-// #include "HTML/Parts/Content_DeviceInfo.htm"
-// #elif TestPage == 4
-// #include "HTML/Parts/Content_I2C.htm"
-// #elif TestPage == 5
-// #include "HTML/Parts/Content_Sonoff.htm"
-// #else
-// #include "HTML/Parts/Content_WTF.htm"
-// #endif
-// #else
-// #include "HTML/Parts/Content_Oops.htm"
-// #endif
-
 #if defined(HomePage)
 const char *HomePagePart =
 #if HomePage == 0
 Content_Verkz;
-#elif TestPage == 1
+#elif HomePage == 1
 Content_Poem;
-#elif TestPage == 2
+#elif HomePage == 2
 Content_SiniLink;
-#elif TestPage == 3
+#elif HomePage == 3
 Content_DeviceInfo;
-#elif TestPage == 4
+#elif HomePage == 4
 Content_I2C;
-#elif TestPage == 5
+#elif HomePage == 5
 Content_Sonoff;
 #else
 Content_WTF;
@@ -92,29 +71,3 @@ Content_WTF;
 Content_Oops;
 #endif
 
-// const char *HomePagePart PROGMEM =
-// #if defined(HomePage)
-// #if HomePage == 0
-// #include "HTML/Parts/Content_Verkz.htm"
-// #elif HomePage == 1
-// #include "HTML/Parts/Content_Poem.htm"
-// #elif HomePage == 2
-// #include "HTML/Parts/Content_SiniLink.htm"
-// #elif HomePage == 3
-// #include "HTML/Parts/Content_DeviceInfo.htm"
-// #elif HomePage == 4
-// #include "HTML/Parts/Content_I2C.htm"
-// #elif HomePage == 5
-// #include "HTML/Parts/Content_Sonoff.htm"
-// #else
-// #include "HTML/Parts/Content_WTF.htm"
-// #endif
-// #else
-
-// #if defined(I2Cenable)
-// #include "HTML/Parts/Content_I2C.htm"
-// #else
-//     "";
-// #endif
-// ;
-// #endif
