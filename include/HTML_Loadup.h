6c5eab3739468648f6f94e1837d8e99d63cb898d
cdntinker <cdntinker@gmail.com>
2021-06-19 15:19:56 -0400

    8

--- include/HTML_Loadup.h
+++ include/HTML_Loadup.h
@@ -93,5 +93,5 @@ ButtonMap Button[] =
 int NumberofButtons = sizeof(Button) / sizeof(Button[0]);
 extern char CurrentPage[32];
 
-extern char* BODY_Test;
+// extern char* BODY_Test;
 /**********************************************************************/
