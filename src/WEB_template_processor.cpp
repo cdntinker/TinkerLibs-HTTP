84e83e320c6c525942afc6f6d2b49a24d42b895c
cdntinker <cdntinker@gmail.com>
2021-07-14 16:25:09 -0400

    0

--- src/WEB_template_processor.cpp
+++ src/WEB_template_processor.cpp
@@ -65,7 +65,8 @@ extern String SiniLink_TurnOn;
 extern String SiniLink_TurnOff;
 #endif
 
-extern const char* TheCSS;
+#include "HTML/Parts/WORKING css.css"
+// extern const char* TheCSS;
 
 String processor(const String &var) // Change placeholders on webpage
 {
