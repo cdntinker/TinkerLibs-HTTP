f1235b60826154328a8e22c9df791236108274f6
cdntinker <cdntinker@gmail.com>
2021-07-20 16:25:31 -0400

    0

--- src/HTTP_functions.cpp
+++ src/HTTP_functions.cpp
@@ -6,12 +6,12 @@
 
 #if defined(ESP8266)
 #include <flash_hal.h>
-// #define MYFS LITTLEFS
+#define MYFS LITTLEFS
 #include <LittleFS.h>
 #elif defined(ESP32)
 #include "Update.h"
-// #define MYFS LITTLEFS
-#include <LittleFS.h>
+#define MYFS LITTLEFS
+#include <LITTLEFS.h>
 #endif
 
 ////////////////////////////////////////////////////
